486b286357b6aa35719cb76671cfb3cd765b9b3a
Caio Samuel <samuel451200@gmail.com>
2026-06-20 21:18:38 -0300

    Adicionar filtro filter_by_tipo e aba Minhas Conquistas

--- users/views.py
+++ users/views.py
@@ -21,7 +21,7 @@ from core.views import (
 )
 from .tables import UserTable
 from .filters import UserFilter
-from presente.models import PerfilGamificado, UsuarioGamificacao
+from presente.models import PerfilGamificado, UsuarioGamificacao, Nivel, TrilhaGamificacao
 from presente.services import PointService
 
 User = get_user_model() 
@@ -90,28 +90,60 @@ class UserProfileView(LoginRequiredMixin, PageTitleMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+            
         # Prefetch social accounts for the current user
         user = User.objects.prefetch_related("socialaccount_set").get(
             pk=self.request.user.pk
         )
+
         context["user"] = user
-        context["my_points"] = PointService.calculate_user_point(self.request.user)
-        context["perfil"] = PerfilGamificado.objects.filter(user=self.request.user).first()
+        my_points = PointService.calculate_user_point(self.request.user)
+        context["my_points"] = my_points
+        perfil = PerfilGamificado.objects.filter(user=self.request.user).first()
+        context["perfil"] = perfil
+        nivel_atual = perfil.nivel if perfil else None
+        
+        if nivel_atual:
+            proximo_nivel = Nivel.objects.filter(pontos_minimos__gt=nivel_atual.pontos_minimos).first()
+        else:
+            proximo_nivel=None
+
+        context["proximo_nivel"] = proximo_nivel
+
+        if not nivel_atual:
+            percentual = 0
+        elif not proximo_nivel:
+            percentual = 100
+        else:
+            percentual = (my_points - nivel_atual.pontos_minimos) * 100 / (proximo_nivel.pontos_minimos - nivel_atual.pontos_minimos)
+        
+        context["percentual"] = percentual
+
+        offset = round(408 * (1 - percentual / 100), 1)
+        context["offset"] = str(offset).replace(",", ".")
+
+        trilhas_completas = TrilhaGamificacao.objects.filter(gamificacao_bonus__usuarios__user=self.request.user)
+        context["trilhas_completas"] = trilhas_completas
+
         context["total_badges"] = UsuarioGamificacao.objects.filter(
             user=self.request.user,
             gamificacao__tipo__tipo="BDG"
         ).count()
+
         context["total_trofeus"] = UsuarioGamificacao.objects.filter(
             user=self.request.user,
             gamificacao__tipo__tipo="TRF"
         ).count()
+
         context["total_medalhas"] = UsuarioGamificacao.objects.filter(
             user=self.request.user,
             gamificacao__tipo__tipo="MDL"
         ).count()
+
         context["conquistas"] = UsuarioGamificacao.objects.filter(
             user=self.request.user
         ).select_related('gamificacao', 'gamificacao__tipo', 'gamificacao__trilha')
+        
         return context
 
 
